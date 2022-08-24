50246a460e36876a91ad22527ceb4d2fc47e08ac
JohnBagshaw <84130776+JohnBagshaw@users.noreply.github.com>
2022-08-24 06:53:31 -0400

    Top level verilog HDL source file for AXI-based IP

---
+++ myipmult_v1_1_toplevel.v
@@ -0,0 +1,84 @@
+
+`timescale 1 ns / 1 ps
+
+	module myipmult_v1_1 #
+	(
+		// Users to add parameters here
+
+		// User parameters ends
+		// Do not modify the parameters beyond this line
+
+
+		// Parameters of Axi Slave Bus Interface S01_AXI
+		parameter integer C_S01_AXI_DATA_WIDTH	= 32,
+		parameter integer C_S01_AXI_ADDR_WIDTH	= 4
+	)
+	(
+		// Users to add ports here
+
+		// User ports ends
+		// Do not modify the ports beyond this line
+
+
+		// Ports of Axi Slave Bus Interface S01_AXI
+		input wire  s01_axi_aclk,
+		input wire  s01_axi_aresetn,
+		input wire [C_S01_AXI_ADDR_WIDTH-1 : 0] s01_axi_awaddr,
+		input wire [2 : 0] s01_axi_awprot,
+		input wire  s01_axi_awvalid,
+		output wire  s01_axi_awready,
+		input wire [C_S01_AXI_DATA_WIDTH-1 : 0] s01_axi_wdata,
+		input wire [(C_S01_AXI_DATA_WIDTH/8)-1 : 0] s01_axi_wstrb,
+		input wire  s01_axi_wvalid,
+		output wire  s01_axi_wready,
+		output wire [1 : 0] s01_axi_bresp,
+		output wire  s01_axi_bvalid,
+		input wire  s01_axi_bready,
+		input wire [C_S01_AXI_ADDR_WIDTH-1 : 0] s01_axi_araddr,
+		input wire [2 : 0] s01_axi_arprot,
+		input wire  s01_axi_arvalid,
+		output wire  s01_axi_arready,
+		output wire [C_S01_AXI_DATA_WIDTH-1 : 0] s01_axi_rdata,
+		output wire [1 : 0] s01_axi_rresp,
+		output wire  s01_axi_rvalid,
+		input wire  s01_axi_rready,
+		output [31:0] axi_aindebug,
+		output [31:0] axi_bindebug,
+		output [63:0] axi_multresdebug
+	);
+// Instantiation of Axi Bus Interface S01_AXI
+	myipmult_v1_1_S01_AXI # ( 
+		.C_S_AXI_DATA_WIDTH(C_S01_AXI_DATA_WIDTH),
+		.C_S_AXI_ADDR_WIDTH(C_S01_AXI_ADDR_WIDTH)
+	) myipmult_v1_1_S01_AXI_inst (
+		.S_AXI_ACLK(s01_axi_aclk),
+		.S_AXI_ARESETN(s01_axi_aresetn),
+		.S_AXI_AWADDR(s01_axi_awaddr),
+		.S_AXI_AWPROT(s01_axi_awprot),
+		.S_AXI_AWVALID(s01_axi_awvalid),
+		.S_AXI_AWREADY(s01_axi_awready),
+		.S_AXI_WDATA(s01_axi_wdata),
+		.S_AXI_WSTRB(s01_axi_wstrb),
+		.S_AXI_WVALID(s01_axi_wvalid),
+		.S_AXI_WREADY(s01_axi_wready),
+		.S_AXI_BRESP(s01_axi_bresp),
+		.S_AXI_BVALID(s01_axi_bvalid),
+		.S_AXI_BREADY(s01_axi_bready),
+		.S_AXI_ARADDR(s01_axi_araddr),
+		.S_AXI_ARPROT(s01_axi_arprot),
+		.S_AXI_ARVALID(s01_axi_arvalid),
+		.S_AXI_ARREADY(s01_axi_arready),
+		.S_AXI_RDATA(s01_axi_rdata),
+		.S_AXI_RRESP(s01_axi_rresp),
+		.S_AXI_RVALID(s01_axi_rvalid),
+		.S_AXI_RREADY(s01_axi_rready),
+		.axi_aindebug(axi_aindebug),
+		.axi_bindebug(axi_bindebug),
+		.axi_multresdebug(axi_multresdebug)
+	);
+
+	// Add user logic here
+
+	// User logic ends
+
+	endmodule
